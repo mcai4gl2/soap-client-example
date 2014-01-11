c5d659ab5135689d6e347763d8e3f1bd893e4488
Geng Li <ligeng0420@gmail.com>
2014-01-11 16:44:45 +0000

    Adding code to print out the weather ids and descriptions.

--- src/main/java/weblog/soap/client/example/TestClient.java
+++ src/main/java/weblog/soap/client/example/TestClient.java
@@ -1,11 +1,14 @@
 package weblog.soap.client.example;
 
 import weblog.soap.client.example.model.GetWeatherInformation;
+import weblog.soap.client.example.model.GetWeatherInformationResponse;
 import weblog.soap.client.example.model.ObjectFactory;
+import weblog.soap.client.example.model.WeatherDescription;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Dispatch;
+import javax.xml.ws.EndpointReference;
 import javax.xml.ws.Service;
 import java.net.URL;
 
@@ -23,6 +26,8 @@ public class TestClient {
                 context,
                 Service.Mode.PAYLOAD);
 
+        dispatch.getRequestContext().put("find.dispatch.operation", Boolean.TRUE);
+
         ObjectFactory factory = new ObjectFactory();
 
         GetWeatherInformation input = new GetWeatherInformation();
@@ -31,6 +36,15 @@ public class TestClient {
 
         Object responseObj = dispatch.invoke(input);
 
-        System.out.println("Got response");
+        System.out.println("Got response:");
+
+        GetWeatherInformationResponse response = (GetWeatherInformationResponse)responseObj;
+
+        for (WeatherDescription description : response.getGetWeatherInformationResult().getWeatherDescription())
+        {
+            System.out.println("Weather Id: " + description.getWeatherID());
+            System.out.println("Weather Description: " + description.getDescription());
+            System.out.println();
+        }
     }
 }
